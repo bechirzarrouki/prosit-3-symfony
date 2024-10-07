47b97c23af5ab63445a1485ee38e49cf81e82938
bechirzarrouki <bechir.zarrouki00@gmail.com>
2024-10-07 14:23:55 +0200

    2

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\AuthorRepository;
 class AuthorController extends AbstractController
-{
+{   
+    public $authors = array(
+    array('id' => 1, 'picture' => '/image/Victor-Hugo.png','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100,'details'=>'http://127.0.0.1:8000/author/authorDetails/1'),
+    array('id' => 2, 'picture' => '/image/william-shakespeare.jfif','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ,'details'=>'http://127.0.0.1:8000/author/authorDetails/2'),
+    array('id' => 3, 'picture' => '/image/Taha_hussein.jfif','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300,'details'=>'http://127.0.0.1:8000/author/authorDetails/3'),
+    );
+
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -16,45 +22,31 @@ class AuthorController extends AbstractController
         ]);
     }
     #[Route('/author/showAuthor/{id}', name: 'apps1_author')]
-    public function showAuthor($id): Response
+    public function showAuthor( $id): Response
     {   
-        $authors = array(
-            array('id' => 1, 'picture' => '/image/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/image/william-shakespeare.jfif','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/image/Taha_hussein.jfif','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
-            );
+        
         return $this->render('author/show.html.twig', [
             'controller_name' => 'AuthorController',
-            'author' => $authors[$id],
+            'author' => $this->authors[$id],
         ]);
     }
     #[Route('/author/listAuthor', name: 'apps2_author')]
-    public function listAuthors(): Response
+    public function listAuthors(AuthorRepository $authorRepository): Response
     {    
-        $authors = array(
-            array('id' => 1, 'picture' => '/image/Victor-Hugo.png','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100,'details'=>'http://127.0.0.1:8000/author/authorDetails/1'),
-            array('id' => 2, 'picture' => '/image/william-shakespeare.jfif','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ,'details'=>'http://127.0.0.1:8000/author/authorDetails/2'),
-            array('id' => 3, 'picture' => '/image/Taha_hussein.jfif','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300,'details'=>'http://127.0.0.1:8000/author/authorDetails/3'),
-            );
-        
+
+        $a=$authorRepository->findAll();
 
         return $this->render('author/list.html.twig', [
             'controller_name' => 'AuthorController',
-            'authors' => $authors,
+            'authors' => $a,
         ]);
     }
     #[Route('/author/authorDetails/{id}', name: 'apps3_author')]
     public function authorDetails($id): Response
     {    
-        $authors = array(
-        array('id' => 1, 'picture' => '/image/Victor-Hugo.png','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100,'details'=>'http://127.0.0.1:8000/author/authorDetails/1'),
-        array('id' => 2, 'picture' => '/image/william-shakespeare.jfif','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ,'details'=>'http://127.0.0.1:8000/author/authorDetails/2'),
-        array('id' => 3, 'picture' => '/image/Taha_hussein.jfif','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300,'details'=>'http://127.0.0.1:8000/author/authorDetails/3'),
-        );
-
         return $this->render('author/showAuthor.html.twig', [
             'controller_name' => 'AuthorController',
-            'author' => $authors[$id-1],
+            'author' => $this->authors[$id-1],
         ]);
     }
 }
